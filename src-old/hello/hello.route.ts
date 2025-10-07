16e6c1c0257e4c6cef52e755d85e85183077dd01
Vinsensius Reinard <vinconloren@gmail.com>
2025-10-07 19:47:18 +0700

    removed old code

--- src-old/hello/hello.route.ts
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const helloWorldRouter = express.Router();
-
-import { handleHelloWorld } from "./hello.controller";
-
-helloWorldRouter.get('/', handleHelloWorld);
-
-export { helloWorldRouter };
--- src-old/v1/user/user.route.ts
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const userRouter = express.Router();
-
-import { handleRegister, handleLogin } from "./user.controller";
-
-userRouter.post('/register', handleRegister);
-userRouter.post('/login', handleLogin);
-
-export { userRouter };
