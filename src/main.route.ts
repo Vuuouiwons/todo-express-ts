b4af65811cdfa8b849d132ce438dd378d278222e
Vinsensius Reinard <vinsensius.reinard@datacomm.co.id>
2025-10-07 11:49:22 +0700

    updated health directory

--- src/main.route.ts
+++ src/main.route.ts
@@ -1,17 +1,17 @@
+import { Request, Response } from 'express';
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const cors = require('cors')
 
-import { handleHealth } from './utils/health';
 import { v1Router } from './v1/v1.route'
 
 const parserMiddleware = [cors(), bodyParser.json(), morgan('dev')]
 const router = express.Router();
-
-router.get('/health', handleHealth);
-
 router.use(parserMiddleware);
+
+router.get('/health', (req: Request, res: Response) => res.status(200).json({ 'status': 'OK' }));
 router.use('/v1', v1Router);
 
 export { router };
--- src/utils/health.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from "express";
-
-const handleHealth = async (req: Request, res: Response) => {
-    return res
-        .status(200)
-        .send({ 'status': 'OK' });
-}
-
-export { handleHealth }
